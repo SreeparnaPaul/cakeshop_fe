a66699ee1f8f4975ecbb48cb0d1dddab65015703
krishnaSarkar5 <krishnasarkar95@gmail.com>
2022-07-21 23:07:06 +0530

    add category form modified

--- src/components/CategoryForm.js
+++ src/components/CategoryForm.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import {Form,Button} from "react-bootstrap"
+ import '../css/Category.css';
 
 
 
@@ -11,17 +12,17 @@ function CategoryForm() {
     
   
     return (
-    <div className="container mt-5">
-         <Form>
+    <div className="container mt-5 category-container">
+         <Form className="form-container">
       <Form.Group className="mb-3" controlId="formBasicCategoryName">
-        <Form.Label>Category Name</Form.Label>
+        <Form.Label className="label">Category Name</Form.Label>
         <Form.Control type="text" placeholder="Enter category name" value={categoryName} onChange={e => {setCategoryName(e.target.value)}} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicDescription">
-        <Form.Label>Category Description</Form.Label>
-        <Form.Control type="textarea" placeholder="About the category" value={categoryDescription} onChange={e => {setCategoryDescription(e.target.value)}}/>
+        <Form.Label className="label">Category Description</Form.Label>
+        <Form.Control as="textarea" rows={4} placeholder="About the category" value={categoryDescription} onChange={e => {setCategoryDescription(e.target.value)}}/>
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button className="button" variant="primary" type="submit">
         Submit
       </Button>
     </Form>
